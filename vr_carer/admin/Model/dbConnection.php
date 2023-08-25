ff79a110e9a3cfa387665b55edadec8479f25c49
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-08-25 21:06:41 +0630

    update

--- vr_carer/admin/Model/dbConnection.php
+++ vr_carer/admin/Model/dbConnection.php
@@ -1,12 +1,11 @@
 <?php
-$hostname = "localhost";
-$port = "3306";
+$hostname = "hosting-beyond-do-user-14484910-0.b.db.ondigitalocean.com";
+$port = "25060";
 $dbname = "vr_carer";
-$username = "root";
-$password = "";
+$username = "doadmin";
+$password = "AVNS_odSp7G5IWykGCI5O0CB";
 $pdo = new PDO(
     "mysql:host=$hostname;port=$port;dbname=$dbname",
     $username,
     $password
 );
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
